99c31468a7fad89312c11dbdf4526d153651e43d
luvkaggarwal@gmail.com <luvkaggarwal>
2019-04-21 18:34:27 -0400

    rectifying 405 error - take 3

--- webapp/caselaw/index.js
+++ webapp/caselaw/index.js
@@ -8,16 +8,38 @@ window.onload = function () {
     }
 };
 
+// function load_data() {
+//     var xhr = new XMLHttpRequest();
+
+//     xhr.onprogress = function () {
+//         alert('LOADING: ', xhr.status);
+//     };
+
+//     xhr.onload = function () {
+//         alert(this.responseText);
+//     };
+//     xhr.open('GET', 'references.txt');
+//     xhr.send();
+// };
+
+const log = document.querySelector('.event-log');
+
+function handleEvent(e) {
+    log.textContent = log.textContent + `${e.type}: ${e.loaded} bytes transferred\n`;
+};
+
+function addListeners(xhr) {
+    xhr.addEventListener('loadstart', handleEvent);
+    xhr.addEventListener('load', handleEvent);
+    xhr.addEventListener('progress', handleEvent);
+};
+
 function load_data() {
-    var xhr = new XMLHttpRequest();
+    log.textContent = '';
 
-    xhr.onprogress = function () {
-        alert('LOADING: ', xhr.status);
-    };
-
-    xhr.onload = function () {
-        alert(this.responseText);
-    };
+    const xhr = new XMLHttpRequest();
+    addListeners(xhr);
     xhr.open('GET', 'references.txt');
     xhr.send();
+    alert(xhr.responseText);  
 };
