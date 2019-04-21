ba8ed3acf83007d588df50788daf81611c1a3493
luvkaggarwal@gmail.com <luvkaggarwal>
2019-04-21 18:04:02 -0400

    Uploading caselaw data

--- webapp/caselaw/index.js
+++ webapp/caselaw/index.js
@@ -4,5 +4,48 @@ window.onload = function () {
         window.location.href = '../index.html';
     } else {
       document.getElementById('greeting').innerText = 'Welcome ' + user;
+      load_data();
     }
 };
+
+function reas() {
+    var r = new FileReader();
+    r.onload = function(e) { 
+        var contents = e.target.result;
+        alert ( "Got the file.n" 
+              + "name: " + f.name + "n"
+              + "type: " + f.type + "n"
+              + "size: " + f.size + " bytesn"
+              + "starts with: " 
+              + contents.substr(1, contents.indexOf("n"))
+        );
+    }
+    alert('all cool');
+    r.readAsText('references.txt');
+    alert('all cool');
+};
+
+function load_data()
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", 'references.txt', false);
+    rawFile.onreadystatechange = function ()
+    {
+        alert('all cool');
+        if(rawFile.readyState === 4)
+        {
+            alert('all cool');
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+};
+
+function rerere() {
+  var data = fetch('references.txt');
+  alert(data);
+}
