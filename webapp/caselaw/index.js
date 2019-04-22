5a60ca096a0369540bce9838a92c2a90032e17b5
luvkaggarwal@gmail.com <luvkaggarwal>
2019-04-22 10:38:46 -0400

    rectifying 405 error - take 12

--- webapp/caselaw/index.js
+++ webapp/caselaw/index.js
@@ -35,37 +35,71 @@
 // }
 
 var myJsonArray;  //Globally defined
-window.onload = function() {
+
+window.onload = function () {
     var user = sessionStorage.getItem("user");
     if ( user == null ) {
         window.location.href = '../index.html';
     } else {
-
-        var outputData = function() {
-            alert(myJsonArray);
-        }
-
-        var myJsonData = function() {
-            var request = new XMLHttpRequest();
-            request.open('GET', 'references.txt', true);
-            request.send(null);
-            request.onreadystatechange = function() {
-                if (request.readyState === 4) {
-                    if (request.status === 200) {
-
-                        myJsonArray = request.responseText;
-                        outputData(); //Ouput when result is received
-                    }
-                }
-
-            } // onreadystatechange
-
-
-        } // var myJsonData
-
-        myJsonData(); // *
+        myJsonData().then(function (result) {
+            console.log("myJsonArray: " + result);
+            alert(result.one);
+            // do something else here
+        });
         document.getElementById('greeting').innerText = 'Welcome ' + user;
         alert('here');
-
     }
-};
+}
+
+function myJsonData() {
+  return new Promise(function (resolve, reject) {
+      var request = new XMLHttpRequest();
+      request.open('GET', 'references.txt', true);
+      //request.setRequestHeader("content-type", "application/json");
+      request.send(null);
+      request.onreadystatechange = function() {
+          if (request.readyState === 4) {
+              if (request.status === 200) {
+                  //var myJsonString = JSON.parse(request.responseText);
+                  //var myJsonArray = myJsonString["Projects"];
+                  resolve(request.responseText);
+              }
+          }
+      } // onreadystatechange
+  });
+}; // var myJsonData
+
+// window.onload = function() {
+//     var user = sessionStorage.getItem("user");
+//     if ( user == null ) {
+//         window.location.href = '../index.html';
+//     } else {
+
+//         var outputData = function() {
+//             alert(myJsonArray);
+//         }
+
+//         var myJsonData = function() {
+//             var request = new XMLHttpRequest();
+//             request.open('GET', 'references.txt', true);
+//             request.send(null);
+//             request.onreadystatechange = function() {
+//                 if (request.readyState === 4) {
+//                     if (request.status === 200) {
+
+//                         myJsonArray = request.responseText;
+//                         outputData(); //Ouput when result is received
+//                     }
+//                 }
+
+//             } // onreadystatechange
+
+
+//         } // var myJsonData
+
+//         myJsonData(); // *
+//         document.getElementById('greeting').innerText = 'Welcome ' + user;
+//         alert('here');
+
+//     }
+// };
