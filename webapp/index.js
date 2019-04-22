0f42ba66c6d1217174842bd7661d734d507a41c7
luvkaggarwal@gmail.com <luvkaggarwal>
2019-04-22 09:59:43 -0400

    rectifying 405 error - take 11

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,7 @@
 function validate(obj) {
     sessionStorage.setItem("user",obj.user.value);
     if ( obj.module.value == "caselaw") {
-        obj.action = "caselaw/index.html";
+        obj.action = "caselaw";
         obj.submit();
     } else if ( obj.module.value == "sentence"){
         obj.action = "sentence/index.html";
