416123626205079e67a3fce0cf89029a6eba6803
luvkaggarwal@gmail.com <luvkaggarwal>
2019-04-21 20:43:18 -0400

    rectifying 405 error - take 10

--- webapp/caselaw/index.js
+++ webapp/caselaw/index.js
@@ -1,69 +1,71 @@
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
+//     var user = sessionStorage.getItem("user");
+//     if ( user == null ) {
+//         window.location.href = '../index.html';
+//     } else {
+//       let result = await load_data();
+//       document.getElementById('greeting').innerText = 'Welcome ' + user;
+//       alert('all cool')
+//       alert(result);
+//     }
+// };
+
+// function load_data() {
+//     return new Promise(function (resolve, reject) {
+//         let xhr = new XMLHttpRequest();
+//         xhr.open('GET', 'references.txt');
+//         xhr.onload = function () {
+//             if (this.status >= 200 && this.status < 300) {
+//                 resolve(xhr.response);
+//             } else {
+//                 reject({
+//                     status: this.status,
+//                     statusText: xhr.statusText
+//                 });
+//             }
+//         };
+//         xhr.onerror = function () {
+//             reject({
+//                 status: this.status,
+//                 statusText: xhr.statusText
+//             });
+//         };
+//         xhr.send();
+//     });
+// }
+
+var myJsonArray;  //Globally defined
+window.onload = function() {
     var user = sessionStorage.getItem("user");
     if ( user == null ) {
         window.location.href = '../index.html';
     } else {
-      let result = await load_data();
-      document.getElementById('greeting').innerText = 'Welcome ' + user;
-      alert(result);
+
+        var outputData = function() {
+            alert(myJsonArray);
+        }
+
+        var myJsonData = function() {
+            var request = new XMLHttpRequest();
+            request.open('GET', 'references.txt', true);
+            request.send(null);
+            request.onreadystatechange = function() {
+                if (request.readyState === 4) {
+                    if (request.status === 200) {
+
+                        myJsonArray = request.responseText;
+                        outputData(); //Ouput when result is received
+                    }
+                }
+
+            } // onreadystatechange
+
+
+        } // var myJsonData
+
+        myJsonData(); // *
+        document.getElementById('greeting').innerText = 'Welcome ' + user;
+        alert('here');
+
     }
 };
-
-function load_data() {
-    return new Promise(function (resolve, reject) {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'references.txt');
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
-            }
-        };
-        xhr.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: xhr.statusText
-            });
-        };
-        xhr.send();
-    });
-}
-
-// const log = document.querySelector('.event-log');
-
-// function handleEvent(e) {
-//     log.textContent = log.textContent + `${e.type}: ${e.loaded} bytes transferred\n`;
-// };
-
-// function addListeners(xhr) {
-//     xhr.addEventListener('loadstart', handleEvent);
-//     xhr.addEventListener('load', handleEvent);
-//     xhr.addEventListener('progress', handleEvent);
-// };
-
-// function load_data() {
-//     log.textContent = '';
-//     const xhr = new XMLHttpRequest();
-//     addListeners(xhr);
-//     xhr.open('GET', 'references.txt');
-//     xhr.send();
-//     alert(xhr.responseText);  
-// };
-
-// function load_data () {
-//     var sr = fetch('references.txt');
-//     alert(sr);
-//     var fileContents = sr.ReadToEnd();
-//     alert('all cool');
-//     sr.Close();
-//     alert('all cool');
-
-//     var mydata = fileContents.Split("\n"[0]);
- 
-//     var myrandom = Random.Range(1,10);
-//     alert(mydata[myrandom]);
-// };
