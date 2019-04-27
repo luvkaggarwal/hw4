f4a6780e25f735f21fbb8f4c8d7ca3f0240c298c
luvkaggarwal@gmail.com <luvkaggarwal>
2019-04-27 17:50:52 -0400

    testing loading random caselaw - take 5

--- webapp/caselaw/index.js
+++ webapp/caselaw/index.js
@@ -63,7 +63,7 @@ function display() {
         ele.setAttribute('type', 'checkbox');
         td.appendChild(ele);
     }
-    console.log('List successfully created.');
+    console.log('List successfully created. Take 5');
 };
 
 function submit(obj) {
